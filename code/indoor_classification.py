4cb5d73fe3719f168fcb68d0746ed8dfde425ebb
wangchen <wangchen@m.scnu.edu.cn>
2018-06-04 16:49:41 +0800

    精简了特征定义和分类中不必要的代码

--- code/indoor_classification.py
+++ code/indoor_classification.py
@@ -5,13 +5,7 @@ from function import *
 
 
 db, cursor = connectdb()
-'''
-print '--------------------feature 1 (stn) svm-----------------------'
-FEATURE1_train_data, train_label = read_feature(db,cursor,traintable,'FEATURE1',train_num)
-FEATURE1_test_data, test_label = read_feature(db,cursor,testtable,'FEATURE1',test_num)
-result = mySVM(FEATURE1_train_data,FEATURE1_test_data,train_label,test_label)
-print 'feature 1 result is: ',result
-'''
+
 
 
 print '--------------------feature 2 (baseline) svm-----------------------'
@@ -25,7 +19,7 @@ fsvm(db=db,cursor=cursor,featurename='FEATURE4')
 fsvm(db=db,cursor=cursor,featurename='FEATURE5')
 
 
-'''
+
 # 3 + 4 + 5 pca
 print '--------------------feature 6 (3+4+5)-----------------------'
 print '从数据库取 FEATURE 6 数据...'
@@ -40,7 +34,7 @@ print FEATURE6_train_data_pca.shape
 print FEATURE6_test_data_pca.shape
 result = mySVM(FEATURE6_train_data_pca, FEATURE6_test_data_pca, train_label, test_label)
 print 'pca feature 6 result is: ',result
-'''
+
 print '--------------------feature 6 (3+4+5)-----------------------'
 print '从数据库取 FEATURE 6 数据...'
 FEATURE3_train_data, train_label = read_feature(db, cursor, table_name=traintable, featurename="FEATURE3",
@@ -63,7 +57,7 @@ print FEATURE6_train_data_pca.shape
 print FEATURE6_test_data_pca.shape
 result = mySVM(FEATURE6_train_data_pca, FEATURE6_test_data_pca, train_label, test_label)
 print 'pca feature 6 result is: ',result
-'''
+
 print '----------------------cam feature 8-----------------------'
 FEATURE8_train_data, train_label = read_feature(db, cursor, traintable, "FEATURE8", train_num)
 FEATURE8_test_data, test_label = read_feature(db, cursor, testtable, "FEATURE8", test_num)
@@ -71,48 +65,4 @@ result = mySVM(FEATURE8_train_data, FEATURE8_test_data, train_label, test_label)
 print 'feature 8 result is: ',result
 
 
-# 3 + 4 + 5 + cam
-print '----------------------"1+2" + cam -------------------------'
-FEATURE9_train_data = np.concatenate((FEATURE6_train_data, FEATURE8_train_data), 1)
-FEATURE9_test_data = np.concatenate((FEATURE6_test_data, FEATURE8_test_data), 1)
-print 'the dimen is :'
-print FEATURE9_train_data.shape
-print FEATURE9_test_data.shape
-#result = mySVM(FEATURE9_train_data, FEATURE9_test_data, train_label, test_label)
-#print 'feature 9 result is: ',result
-
-
-# 特征融合 pca
-FEATURE10_train_data,FEATURE10_test_data = myPCA(FEATURE9_train_data,FEATURE9_test_data)
-print 'after pca , the dime is:'
-print FEATURE10_train_data.shape
-print FEATURE10_test_data.shape
-result = mySVM(FEATURE10_train_data, FEATURE10_test_data, train_label, test_label)
-print 'the result is: ',result
-
-
-# lsq 特征融合 pca
-print '---------------------lsq  "1+2" + cam ----------------------'
-FEATURE7_train_data, train_label = read_feature(db, cursor, traintable, "FEATURE7", train_num)
-FEATURE7_test_data, test_label = read_feature(db, cursor, testtable, "FEATURE7", test_num)
-result = mySVM(FEATURE7_train_data, FEATURE7_test_data, train_label, test_label)
-print 'lsq_cam result is :',result
-FEATURE11_train_data = np.concatenate((FEATURE6_train_data, FEATURE7_train_data), 1)
-FEATURE11_test_data = np.concatenate((FEATURE6_test_data, FEATURE7_test_data), 1)
-print 'the dimen is :'
-print FEATURE11_train_data.shape
-print FEATURE11_test_data.shape
-#result = mySVM(FEATURE11_train_data, FEATURE11_test_data, train_label, test_label)
-FEATURE12_train_data,FEATURE12_test_data = myPCA(FEATURE11_train_data,FEATURE11_test_data)
-result = mySVM(FEATURE12_train_data, FEATURE12_test_data, train_label, test_label)
-print 'feature 12 result is: ',result
-
-# stn + '1+2'
-print '----------------------stn + "1+2" -------------------------'
-FEATURE13_train_data = np.concatenate((FEATURE1_train_data, FEATURE6_train_data), 1)
-FEATURE13_test_data = np.concatenate((FEATURE1_test_data, FEATURE6_test_data), 1)
-FEATURE13_train_data,FEATURE13_test_data = myPCA(FEATURE13_train_data,FEATURE13_test_data)
-result = mySVM(FEATURE13_train_data, FEATURE13_test_data, train_label, test_label)
-print 'feature 13 result is: ',result
-'''
 closedb(db)
